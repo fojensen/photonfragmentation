0a5e16d01992cff52c0179a3c032f6cb1ed3b703
Frank Jensen <frank.jensen@colorado.edu>
2018-12-07 06:48:04 -0600

    add ratio for madMin...

--- labelCMS.h
+++ labelCMS.h
@@ -1,10 +1,14 @@
 #include <TPaveText.h>
 
-void labelCMS()
+void labelCMS(const bool showlumi = false)
 {
    TPaveText * label0 = new TPaveText(0.3, .905, 0.85, .945, "NDC, NB");
    label0->SetFillColor(0);
    label0->SetTextAlign(31);
-   label0->AddText("CMS Simulation  #it{Preliminary}   13 TeV");
+   if (showlumi) {
+      label0->AddText("CMS Simulation  #it{Preliminary}   35.9 fb^{1}  13 TeV");
+   } else {
+      label0->AddText("CMS Simulation  #it{Preliminary}   13 TeV");
+   }
    label0->Draw();
 }
--- madMinPhotonDeltaR.c
+++ madMinPhotonDeltaR.c
@@ -8,15 +8,15 @@
 
 void madMinPhotonDeltaR()
 {
-   TChain * chain_Zinv = new TChain("tree");
+   TChain * chain_ZJetsToNuNu = new TChain("tree");
    const TString sigdir = "/eos/uscms/store/user/lpcsusyhad/SusyRA2Analysis2015/Skims/Run2ProductionV12/tree_signal/";
-   chain_Zinv->Add(sigdir + "tree_ZJetsToNuNu_HT-100to200.root");
-   chain_Zinv->Add(sigdir + "tree_ZJetsToNuNu_HT-200to400.root");
-   chain_Zinv->Add(sigdir + "tree_ZJetsToNuNu_HT-400to600.root");
-   chain_Zinv->Add(sigdir + "tree_ZJetsToNuNu_HT-600to800.root");
-   chain_Zinv->Add(sigdir + "tree_ZJetsToNuNu_HT-800to1200.root");
-   chain_Zinv->Add(sigdir + "tree_ZJetsToNuNu_HT-1200to2500.root");
-   chain_Zinv->Add(sigdir + "tree_ZJetsToNuNu_HT-2500toInf.root");
+   chain_ZJetsToNuNu->Add(sigdir + "tree_ZJetsToNuNu_HT-100to200.root");
+   chain_ZJetsToNuNu->Add(sigdir + "tree_ZJetsToNuNu_HT-200to400.root");
+   chain_ZJetsToNuNu->Add(sigdir + "tree_ZJetsToNuNu_HT-400to600.root");
+   chain_ZJetsToNuNu->Add(sigdir + "tree_ZJetsToNuNu_HT-600to800.root");
+   chain_ZJetsToNuNu->Add(sigdir + "tree_ZJetsToNuNu_HT-800to1200.root");
+   chain_ZJetsToNuNu->Add(sigdir + "tree_ZJetsToNuNu_HT-1200to2500.root");
+   chain_ZJetsToNuNu->Add(sigdir + "tree_ZJetsToNuNu_HT-2500toInf.root");
    
    const TString dir = "/eos/uscms/store/user/lpcsusyhad/SusyRA2Analysis2015/Skims/Run2ProductionV12/tree_GJet_CleanVars/";
 
@@ -35,76 +35,116 @@ void madMinPhotonDeltaR()
    chain_QCD->Add(dir + "tree_QCD_HT-1500to2000.root");
    chain_QCD->Add(dir + "tree_QCD_HT-2000toInf.root");
 
-   TChain * chain_GJets0p4 = new TChain("tree");
-   chain_GJets0p4->Add(dir + "tree_GJets_DR-0p4_HT-100to200.root");
-   chain_GJets0p4->Add(dir + "tree_GJets_DR-0p4_HT-200to400.root");
-   chain_GJets0p4->Add(dir + "tree_GJets_DR-0p4_HT-400to600.root");
-   chain_GJets0p4->Add(dir + "tree_GJets_DR-0p4_HT-600toInf.root");
+   TChain * chain_GJets_DR0p4 = new TChain("tree");
+   chain_GJets_DR0p4->Add(dir + "tree_GJets_DR-0p4_HT-100to200.root");
+   chain_GJets_DR0p4->Add(dir + "tree_GJets_DR-0p4_HT-200to400.root");
+   chain_GJets_DR0p4->Add(dir + "tree_GJets_DR-0p4_HT-400to600.root");
+   chain_GJets_DR0p4->Add(dir + "tree_GJets_DR-0p4_HT-600toInf.root");
  
-   TH1D * hist = new TH1D("hist", ";min #DeltaR (#gamma, hard partons);events / 0.05", 16, 0., 0.8);
+   TH1D * h = new TH1D("h", ";min #DeltaR (#gamma, hard partons);events / 0.05", 16, 0., 0.8);
    const double max = 2000.;
    const TString savetag = "narrowrange";
 
-   //TH1D * hist = new TH1D("hist", ";min #DeltaR (#gamma, hard partons);events / 0.1", 40, 0., 4.);
+   //TH1D * h = new TH1D("h", ";min #DeltaR (#gamma, hard partons);events / 0.1", 40, 0., 4.);
    //const double max = 8000.;
    //const TString savetag = "widerange";
 
-   hist->SetLineWidth(2);
-   TH1D * dr_Zinv = (TH1D*)hist->Clone("dr_Zinv");
-   TH1D * dr_GJets = (TH1D*)hist->Clone("dr_GJets");
-   TH1D * dr_GJets0p4 = (TH1D*)hist->Clone("dr_GJets0p4");
-   TH1D * dr_QCD = (TH1D*)hist->Clone("dr_QCD");
+   h->SetLineWidth(2);
+   h->SetStats(0);
+   h->Sumw2();
+   TH1D * h_ZJetsToNuNu = (TH1D*)h->Clone("h_ZJetsToNuNu");
+   TH1D * h_GJets = (TH1D*)h->Clone("h_GJets");
+   TH1D * h_GJets_DR0p4 = (TH1D*)h->Clone("h_GJets_DR0p4");
+   TH1D * h_QCD = (TH1D*)h->Clone("h_QCD");
 
-   const TCut cuts_photon = "@Photons->size()==1 && Photons[0].Pt()>=200. && Photons_fullID[0]==1";
-   const TCut cuts_leptons = "@Electrons->size()==0 && @Muons->size()==0"; 
-   const TCut cuts_tracks = "isoElectronTracks==0 && isoMuonTracks==0 && isoPionTracks==0";
-   const TCut cuts_dphi = "DeltaPhi1>=0.5 && DeltaPhi2>=0.5 && DeltaPhi3>=0.3 && DeltaPhi4>=0.3";
-   TCut cuts_baseline = "NJets>=2 && HT>=300. && MHT>=300. && JetID==1 && BTags==0";
-   cuts_baseline = cuts_leptons && cuts_tracks && cuts_dphi && cuts_baseline;
+   const TCut cut_photon = "@Photons->size()==1 && Photons[0].Pt()>=200. && Photons_fullID[0]==1";
+   const TCut cut_leptons = "@Electrons->size()==0 && @Muons->size()==0"; 
+   const TCut cut_tracks = "isoElectronTracks==0 && isoMuonTracks==0 && isoPionTracks==0";
+   const TCut cut_dphi = "DeltaPhi1>=0.5 && DeltaPhi2>=0.5 && DeltaPhi3>=0.3 && DeltaPhi4>=0.3";
+   TCut cut_baseline = "NJets>=2 && HT>=300. && MHT>=300. && JetID==1 && BTags==0";
+   cut_baseline = cut_leptons && cut_tracks && cut_dphi && cut_baseline;
    
-   char cuts_Zinv[2000];
-   sprintf(cuts_Zinv, "Weight * 35862.824 * (%s)", TString(cuts_baseline).Data());
-   std::cout << "filling from Zinv..." << std::endl;
-   chain_Zinv->Project("dr_Zinv", "madMinPhotonDeltaR", cuts_Zinv);
+   char cut_ZJetsToNuNu[2000];
+   sprintf(cut_ZJetsToNuNu, "Weight * 35862.824 * (%s)", TString(cut_baseline).Data());
+   std::cout << "filling from ZJetsToNuNu..." << std::endl;
+   chain_ZJetsToNuNu->Project("h_ZJetsToNuNu", "madMinPhotonDeltaR", cut_ZJetsToNuNu);
 
-   char cuts_GJets[2000];
-   sprintf(cuts_GJets, "Weight * 35862.824 * (%s)", TString(cuts_baseline && cuts_photon).Data());
+   char cut_GJets[2000];
+   sprintf(cut_GJets, "Weight * 35862.824 * (%s)", TString(cut_baseline && cut_photon).Data());
    std::cout << "filling from GJets..." << std::endl;
-   chain_GJets->Project("dr_GJets", "madMinPhotonDeltaR", cuts_GJets);
-   std::cout << "filling from GJets0p4..." << std::endl;
-   chain_GJets0p4->Project("dr_GJets0p4", "madMinPhotonDeltaR", cuts_GJets);
+   chain_GJets->Project("h_GJets", "madMinPhotonDeltaR", cut_GJets);
+   std::cout << "filling from GJets_DR-0p4..." << std::endl;
+   chain_GJets_DR0p4->Project("h_GJets_DR0p4", "madMinPhotonDeltaR", cut_GJets);
 
-   char cuts_QCD[2000];
-   sprintf(cuts_QCD, "Weight * 35862.824 * (%s && madMinDeltaRStatus==1)", TString(cuts_baseline && cuts_photon).Data());
+   char cut_QCD[2000];
+   sprintf(cut_QCD, "Weight * 35862.824 * (%s && madMinDeltaRStatus==1)", TString(cut_baseline && cut_photon).Data());
    std::cout << "filling from QCD..." << std::endl;
-   chain_QCD->Project("dr_QCD", "madMinPhotonDeltaR", cuts_QCD);
+   chain_QCD->Project("h_QCD", "madMinPhotonDeltaR", cut_QCD);
 
-   TCanvas * c = new TCanvas("c", "", 400., 400.);
-   dr_GJets->SetLineColor(8);
-   dr_GJets0p4->SetLineColor(7);
-   dr_QCD->SetLineColor(6);
-   dr_Zinv->SetLineColor(9);
-   dr_Zinv->SetLineStyle(2);
+   TCanvas * can_h = new TCanvas("can_h", "", 400., 400.);
+   h_GJets->SetLineColor(8);
+   h_GJets_DR0p4->SetLineColor(7);
+   h_QCD->SetLineColor(6);
+   h_ZJetsToNuNu->SetLineColor(9);
+   h_ZJetsToNuNu->SetLineStyle(2);
 
-   dr_GJets->Draw("HIST, E");
-   dr_GJets->SetMinimum(0.);
-   dr_GJets->SetMaximum(max);
-   dr_GJets->SetStats(0);
-   dr_GJets0p4->Draw("HIST, E, SAME");
-   dr_QCD->Draw("HIST, E, SAME");
-   dr_Zinv->Draw("HIST, E, SAME");
+   h_GJets->Draw("HIST, E");
+   h_GJets->SetMinimum(0.);
+   h_GJets->SetMaximum(max);
+   h_GJets->SetStats(0);
+   h_GJets_DR0p4->Draw("HIST, E, SAME");
+   h_QCD->Draw("HIST, E, SAME");
+   h_ZJetsToNuNu->Draw("HIST, E, SAME");
 
-   TLegend * l = new TLegend(0.225, 0.75, 0.865, 0.89);
-   l->SetNColumns(2);
-   l->SetBorderSize(0);
-   l->AddEntry(dr_GJets, "GJets", "L");
-   l->AddEntry(dr_GJets0p4, "GJets_DR-0p4", "L");
-   l->AddEntry(dr_QCD, "QCD", "L");
-   l->AddEntry(dr_Zinv, "ZJetsToNuNu", "L");
-   l->Draw();
+   TLegend * leg_h = new TLegend(0.225, 0.75, 0.865, 0.89);
+   leg_h->SetNColumns(2);
+   leg_h->SetBorderSize(0);
+   leg_h->AddEntry(h_GJets, "GJets", "L");
+   leg_h->AddEntry(h_GJets_DR0p4, "GJets_DR-0p4", "L");
+   leg_h->AddEntry(h_QCD, "QCD", "L");
+   leg_h->AddEntry(h_ZJetsToNuNu, "ZJetsToNuNu", "L");
+   leg_h->Draw();
+
+   labelCMS(true);
+   can_h->SaveAs("./plots/madMinPhotonDeltaR."+savetag+".pdf");
+
+   TH1D * r_GJets = (TH1D*)h_ZJetsToNuNu->Clone("r_GJets");
+   r_GJets->Divide(h_GJets);
+
+   TH1D * r_GJets_DR0p4 = (TH1D*)h_ZJetsToNuNu->Clone("r_GJets_DR0p4");
+   r_GJets_DR0p4->Divide(h_GJets_DR0p4);
+
+   TH1D * r_QCD = (TH1D*)h_ZJetsToNuNu->Clone("r_QCD");
+   r_QCD->Divide(h_QCD);
+
+   TCanvas * can_r = new TCanvas("can_r", "", 400, 400);  
+   r_GJets->SetMarkerColor(8);
+   r_GJets->SetLineColor(8);
+   r_GJets->SetMarkerStyle(20);
+   r_GJets_DR0p4->SetMarkerColor(7);
+   r_GJets_DR0p4->SetLineColor(7);
+   r_GJets_DR0p4->SetMarkerStyle(20);
+   r_QCD->SetMarkerColor(6);
+   r_QCD->SetLineColor(6);
+   r_QCD->SetMarkerStyle(20);
+
+   r_GJets->Draw("PE");
+   r_GJets->SetTitle(";min #DeltaR (#gamma, hard partons);ZJetsToNuNu ratio");
+   r_GJets_DR0p4->Draw("PE, SAME");
+   r_QCD->Draw("PE, SAME");
+   r_GJets->SetMinimum(0.01);
+   r_GJets->SetMaximum(10.);
+   can_r->SetLogy();
+
+   TLegend * leg_r = new TLegend(0.225, 0.15, 0.865, 0.34);
+   leg_r->SetNColumns(3);
+   leg_r->SetBorderSize(0);
+   leg_r->AddEntry(r_GJets, "GJets", "P");
+   leg_r->AddEntry(r_GJets_DR0p4, "GJets_DR-0p4", "P");
+   leg_r->AddEntry(r_QCD, "QCD", "P");
+   leg_r->Draw();
 
    labelCMS();
-
-   c->SaveAs("./plots/madMinPhotonDeltaR."+savetag+".pdf");
+   can_r->SaveAs("./plots/madMinPhotonDeltaR.ratio."+savetag+".pdf");
 }
 
